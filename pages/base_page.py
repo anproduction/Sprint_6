711bb1becb2d807bbb5b9f9691cd43ca074c69d5
Anastasia Prodan <prodananastasija@yandex.com>
2025-05-22 01:23:52 +0300

    add_pages

---
+++ pages/base_page.py
@@ -0,0 +1,82 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import allure
+
+
+class BasePage:
+    def __init__(self, driver, timeout=60):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, timeout)
+
+    @allure.step("Открытие страницы")
+    def open(self, url):
+        self.driver.get(url)
+
+    @allure.step("Поиск элемента с локатором")
+    def find_element(self, *locator):
+        return self.driver.find_element(*locator)
+
+    @allure.step("Поиск множества элементов с локатором")
+    def find_elements(self, *locator):
+        return self.driver.find_elements(*locator)
+
+    @allure.step("Ожидание видимости элемента с локатором")
+    def wait_for_element_to_be_visible(self, locator):
+        return self.wait.until(EC.visibility_of_element_located(locator))
+
+    @allure.step("Ожидание кликабельности элемента с локатором")
+    def wait_for_element_to_be_clickable(self, locator):
+        return self.wait.until(EC.element_to_be_clickable(locator))
+
+    @allure.step("Получение текста элемента")
+    def get_text(self, *locator):
+        element = self.find_element(*locator)
+        return element.text
+
+    @allure.step("Клик по элементу")
+    def click(self, locator):
+        self.wait_for_element_to_be_visible(locator)
+        element = self.driver.find_element(*locator)
+        element.click()
+
+    @allure.step("Ввод текста в элемент")
+    def send_keys(self, locator, text):
+        self.wait_for_element_to_be_visible(locator)
+        element = self.driver.find_element(*locator)
+        element.clear()
+        element.send_keys(text)
+
+    @allure.step("Клик по элементу с ожиданием видимости")
+    def click_with_wait(self, locator):
+        self.wait_for_element_to_be_visible(locator)
+        self.click(locator)
+
+    @allure.step("Получить текст с элемента")
+    def get_text_with_wait(self, locator):
+        self.wait_for_element_to_be_visible(locator)
+        return self.get_text(locator)
+
+    # Добавленные методы
+
+    @allure.step("Поиск элемента с ожиданием видимости")
+    def find_element_with_wait(self, *locator):
+        self.wait_for_element_to_be_visible(locator)
+        return self.find_element(*locator)
+
+    @allure.step("Проверка отображения элемента")
+    def check_displaying_of_element(self, *locator):
+        element = self.find_element(*locator)
+        assert element.is_displayed(), f"Элемент {locator} не отображается"
+
+    @allure.step("Скролл до элемента")
+    def scroll_to_element(self, locator):
+        element = self.wait_for_element_to_be_visible(locator)
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+
+    @allure.step("Переключение на следующую вкладку")
+    def switch_to_next_tab(self):
+        self.driver.switch_to.window(self.driver.window_handles[1])
+
+    @allure.step("Ожидание URL")
+    def wait_url_to_be(self, url):
+        return self.wait.until(EC.url_to_be(url))
---
+++ pages/home_page.py
@@ -0,0 +1,47 @@
+import allure
+from locators.home_page_locators import HomePageLocators
+from pages.base_page import BasePage
+from urls import URLs
+
+
+class HomePage(BasePage):
+
+    @allure.step('Нажатие кнопки "Заказать" в хедере')
+    def click_order_button_header(self):
+        self.click_with_wait(HomePageLocators.ORDER_BUTTON_HEADER)
+
+    @allure.step('Скролл до кнопки "Заказать" в теле страницы')
+    def scroll_to_order_button_body(self):
+        self.scroll_to_element(HomePageLocators.HOW_IT_WORKS_BLOCK)
+        self.wait_for_element_to_be_visible(HomePageLocators.ORDER_BUTTON_BODY)
+
+    @allure.step('Нажатие кнопки "Заказать" в теле страницы')
+    def click_order_button_body(self):
+        self.click_with_wait(HomePageLocators.ORDER_BUTTON_BODY)
+
+    @allure.step('Закрытие окна куки')
+    def close_cookie_window(self):
+        self.click_with_wait(HomePageLocators.ACCEPT_COOKIE_BUTTON)
+
+    @allure.step('Скролл до блока "Вопросы о важном"')
+    def scroll_to_faq(self):
+        self.scroll_to_element(HomePageLocators.FAQ_ITEMS[7]["question"])
+        self.wait_for_element_to_be_visible(HomePageLocators.FAQ_ITEMS[7]["question"])
+
+    @allure.step('Нажатие на вопрос')
+    def click_the_question(self, question_locator):
+        self.click_with_wait(question_locator)
+
+    @allure.step('Получение текста ответа')
+    def get_the_answer_text(self, answer_locator):
+        return self.get_text_with_wait(answer_locator)
+
+    @allure.step('Нажатие на логотип "Яндекс"')
+    def click_logo_yandex_open_dzen_page(self):
+        self.click_with_wait(HomePageLocators.LOGO_YANDEX)
+        self.switch_to_next_tab()
+        self.wait_url_to_be(URLs.YANDEX_URL)
+
+    @allure.step('Нажатие на логотип "Самокат"')
+    def click_logo_open_home_page(self):
+        self.click_with_wait(HomePageLocators.LOGO_SAMOKAT)

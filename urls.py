05784dc9e938dda9077022794243b12a7163756d
Anastasia Prodan <prodananastasija@yandex.com>
2025-05-22 00:49:17 +0300

    add_urls

---
+++ urls.py
@@ -0,0 +1,4 @@
+class URLs:
+    YANDEX_URL = 'https://dzen.ru/?yredirect=true'
+    SAMOKAT_URL = 'https://qa-scooter.praktikum-services.ru/'
+    BASE_URL = 'https://qa-scooter.praktikum-services.ru/'

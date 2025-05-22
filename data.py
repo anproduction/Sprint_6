f93aeaa6af56577707a12e7b2fcd2f039bc543d0
Anastasia Prodan <prodananastasija@yandex.com>
2025-05-22 22:11:43 +0300

    add_data

---
+++ data.py
@@ -0,0 +1,23 @@
+
+FAQ_QUESTIONS_COUNT = 8
+
+ORDER_USER_DATA = [
+    {
+        "first_name": "Иван",
+        "last_name": "Иванов",
+        "address": "ул. Пушкина, д. 10",
+        "station": "Технопарк",
+        "phone": "+79270000001",
+        "comment": "Позвонить за 5 минут"
+    },
+    {
+        "first_name": "Мария",
+        "last_name": "Петрова",
+        "address": "пр. Ленина, д. 5",
+        "station": "Курская",
+        "phone": "+79270000002",
+        "comment": "Без звонка"
+    }
+]
+
+ENTRY_POINTS = ["header", "body"]

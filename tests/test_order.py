2ab4ec8762799494ec19bbbc3fdf2bccdd58b760
Anastasia Prodan <prodananastasija@yandex.com>
2025-05-25 22:45:50 +0300

    fix_classes_to_tests

--- tests/test_order.py
+++ tests/test_order.py
@@ -5,70 +5,71 @@ from data import ORDER_USERS
 from urls import URLs
 
 
-@pytest.mark.parametrize("user_data", ORDER_USERS)
-def test_successful_order_from_header(driver, user_data):
-    home_page = HomePage(driver)
-    order_form = OrderFormPage(driver)
+class TestSuccessfulOrder:
 
-    home_page.open(URLs.BASE_URL)
-    home_page.close_cookie_window()
-    home_page.click_order_button_header()
+    @pytest.mark.parametrize("user_data", ORDER_USERS)
+    def test_successful_order_from_header(self, driver, user_data):
+        home_page = HomePage(driver)
+        order_form = OrderFormPage(driver)
 
-    order_form.personal_information_input(
-        name=user_data[0],
-        last_name=user_data[1],
-        address=user_data[2],
-        station=user_data[3],
-        number=user_data[4]
-    )
-    order_form.rental_information_input(comment=user_data[5])
-    order_form.click_yes_button_confirmation_pop_up()
-    order_form.check_displaying_of_confirm_window()
-    order_form.close_complete_order_popup()
+        home_page.open(URLs.BASE_URL)
+        home_page.close_cookie_window()
+        home_page.click_order_button_header()
 
-    home_page.click_logo_open_home_page()
-    home_page.wait_url_to_be(URLs.BASE_URL)
-    assert home_page.get_current_url() == URLs.BASE_URL, "Переход по логотипу Самоката на главную страницу"
+        order_form.personal_information_input(
+            name=user_data[0],
+            last_name=user_data[1],
+            address=user_data[2],
+            station=user_data[3],
+            number=user_data[4]
+        )
+        order_form.rental_information_input(comment=user_data[5])
+        order_form.click_yes_button_confirmation_pop_up()
+        order_form.check_displaying_of_confirm_window()
+        order_form.close_complete_order_popup()
 
-    home_page.click_logo_yandex_open_dzen_page()
-    handles = driver.window_handles
-    driver.switch_to.window(handles[-1])
-    home_page.wait_url_to_be(URLs.YANDEX_URL)
-    assert home_page.get_current_url() == URLs.YANDEX_URL, "Переход по логотипу Яндекса на страницу Дзена"
-    driver.close()
-    driver.switch_to.window(handles[0])
+        home_page.click_logo_open_home_page()
+        home_page.wait_url_to_be(URLs.BASE_URL)
+        assert home_page.get_current_url() == URLs.BASE_URL, "Переход по логотипу Самоката на главную страницу"
 
+        home_page.click_logo_yandex_open_dzen_page()
+        handles = driver.window_handles
+        driver.switch_to.window(handles[-1])
+        home_page.wait_url_to_be(URLs.YANDEX_URL)
+        assert home_page.get_current_url() == URLs.YANDEX_URL, "Переход по логотипу Яндекса на страницу Дзена"
+        driver.close()
+        driver.switch_to.window(handles[0])
 
-@pytest.mark.parametrize("user_data", ORDER_USERS)
-def test_successful_order_from_body(driver, user_data):
-    home_page = HomePage(driver)
-    order_form = OrderFormPage(driver)
+    @pytest.mark.parametrize("user_data", ORDER_USERS)
+    def test_successful_order_from_body(self, driver, user_data):
+        home_page = HomePage(driver)
+        order_form = OrderFormPage(driver)
 
-    home_page.open(URLs.BASE_URL)
-    home_page.close_cookie_window()
-    home_page.scroll_to_order_button_body()
-    home_page.click_order_button_body()
+        home_page.open(URLs.BASE_URL)
+        home_page.close_cookie_window()
+        home_page.scroll_to_order_button_body()
+        home_page.click_order_button_body()
 
-    order_form.personal_information_input(
-        name=user_data[0],
-        last_name=user_data[1],
-        address=user_data[2],
-        station=user_data[3],
-        number=user_data[4]
-    )
-    order_form.rental_information_input(comment=user_data[5])
-    order_form.click_yes_button_confirmation_pop_up()
-    order_form.check_displaying_of_confirm_window()
-    order_form.close_complete_order_popup()
+        order_form.personal_information_input(
+            name=user_data[0],
+            last_name=user_data[1],
+            address=user_data[2],
+            station=user_data[3],
+            number=user_data[4]
+        )
+        order_form.rental_information_input(comment=user_data[5])
+        order_form.click_yes_button_confirmation_pop_up()
+        order_form.check_displaying_of_confirm_window()
+        order_form.close_complete_order_popup()
 
-    home_page.click_logo_open_home_page()
-    home_page.wait_url_to_be(URLs.BASE_URL)
-    assert home_page.get_current_url() == URLs.BASE_URL, "Переход по логотипу Самоката на главную страницу"
+        home_page.click_logo_open_home_page()
+        home_page.wait_url_to_be(URLs.BASE_URL)
+        assert home_page.get_current_url() == URLs.BASE_URL, "Переход по логотипу Самоката на главную страницу"
 
-    home_page.click_logo_yandex_open_dzen_page()
-    handles = driver.window_handles
-    driver.switch_to.window(handles[-1])
-    home_page.wait_url_to_be(URLs.YANDEX_URL)
-    assert home_page.get_current_url() == URLs.YANDEX_URL, "Переход по логотипу Яндекса на страницу Дзена"
-    driver.close()
-    driver.switch_to.window(handles[0])
+        home_page.click_logo_yandex_open_dzen_page()
+        handles = driver.window_handles
+        driver.switch_to.window(handles[-1])
+        home_page.wait_url_to_be(URLs.YANDEX_URL)
+        assert home_page.get_current_url() == URLs.YANDEX_URL, "Переход по логотипу Яндекса на страницу Дзена"
+        driver.close()
+        driver.switch_to.window(handles[0])
